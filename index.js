e6b9f7aa58ae601a97d26f331cdccf215440742b
toriz7 <toriz7@naver.com>
2021-03-17 00:27:39 +0900

    express

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
+const admin=require('./routes/admin')
 const app=express();
 const port=3000;
 app.get('/',(req,res)=>{
     res.send("hello world! Express");
 });
+
+app.use('/admin',admin); //요건 미들웨어
 app.listen(port, () =>{
     console.log("Express Listening on port",port);
 })
