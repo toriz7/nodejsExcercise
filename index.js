07692de4e792d7b0d6879203511480f2f1b5b685
toriz7 <toriz7@naver.com>
2021-03-17 22:56:42 +0900

    template

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express=require('express');
 const admin=require('./routes/admin') // admin 아래의 url 을 접속하기 위한 변수
+const nunjucks=require('nunjucks');
+
 const app=express();
 const port=3000;
+
+
+app.use('/admin',admin); //요건 미들웨어. 
+
+nunjucks.configure('template',{
+    autoescape: true, //html, javascript 오류 공격시 태그. 크로스사이트 공격 방어.
+    express:app //express 변수인 app 선언
+});
+
 app.get('/',(req,res)=>{
     res.send("hello world! Express");
 });
 
-app.use('/admin',admin); //요건 미들웨어
 app.listen(port, () =>{
     console.log("Express Listening on port",port);
 })
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,16 @@
 const express=require('express');
+
 const router=express.Router();
 
 router.get('/',(req,res)=>{
     res.send("admin 이후 url");
 })
+
 router.get('/products',(req,res)=>{
-    res.send("admin products")
+    //res.send("admin/products");
+    res.render("admin/products.html",{
+        message: 'products hello!',
+        online : "hello products!"
+    })
 })
 module.exports=router;
