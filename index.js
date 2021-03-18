546810d12495823b93e3425c9c30ca17f7de49f4
toriz7 <toriz7@naver.com>
2021-03-18 23:48:20 +0900

    temp

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express=require('express');
 const admin=require('./routes/admin') // admin 아래의 url 을 접속하기 위한 변수
 const nunjucks=require('nunjucks');
+const logger = require('morgan');
 
 const app=express();
 const port=3000;
 
 
-app.use('/admin',admin); //요건 미들웨어. 
+app.use('/admin',vipMiddleware,admin); //요건 미들웨어. 
+//middleware setting
+app.use(logger('dev'));
+
 
 nunjucks.configure('template',{
     autoescape: true, //html, javascript 오류 공격시 태그. 크로스사이트 공격 방어.
     express:app //express 변수인 app 선언
 });
+
+function vipMiddleware(req,res,next){
+    console.log('vip middle ware');
+    next();
+}
 
 app.get('/',(req,res)=>{
     res.send("hello world! Express");
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,35 @@
 const express=require('express');
 
 const router=express.Router();
+// 여기서 로그인 인자는 3개를 받는다. req, res, next
 
-router.get('/',(req,res)=>{
+
+function testMiddleware(req,res,next){
+    console.log('first middle ware');
+    next();
+}
+
+function testMiddleware2(req,res,next){
+    console.log('second middle ware');
+    next();
+}
+/*
+router.get('/', testMiddleware,testMiddleware2,(req,res)=>{
     res.send("admin 이후 url");
 })
-
+*/
+router.get('/', testMiddleware,testMiddleware2,(req,res)=>{
+    res.send("admin 이후 url");
+})
 router.get('/products',(req,res)=>{
     //res.send("admin/products");
     res.render("admin/products.html",{
         message: 'products hello!',
         online : "hello products!"
     })
+})
+
+router.get('/products/write',(req,res)=>{
+    res.render("admin/write.html");
 })
 module.exports=router;
